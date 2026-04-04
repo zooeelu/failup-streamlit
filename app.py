724f07a9d6c524ede294119dd191be5f8953b464
Zoe Lu <zoelu0129@gmail.com>
2026-04-04 19:39:51 -0400

    Embed HTML prototype in Streamlit

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 st.set_page_config(page_title="FailUp", layout="wide")
 
-st.title("FailUp")
-st.write("Use the sidebar to navigate.")
+with open("failup.html", "r", encoding="utf-8") as f:
+    html_content = f.read()
+
+components.html(html_content, height=1200, scrolling=True)
